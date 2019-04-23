dc26a26ff52576c7e7a780061eb7ae272cc1a485
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-23 10:15:15 -0500

    start to unify output

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-import time
+import time #required for sleep
 print("hello world")
+print("python")
 for i in range(1,4):
-    print chr(7)
+#    print  chr(7)
+    print(" beep!\a")
     time.sleep(1)
     
