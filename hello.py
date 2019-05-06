55a8875e8445fc317d55bccb0c5b5f98f705f367
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-06 11:25:12 -0500

    add iteration number

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ import time #required for sleep
 print("hello world")
 print("python")
 for i in range(1,4): #print beeps at end of program
-    print(" beep!\a")
+    print(" %d beeps\a") %i
     time.sleep(1)
