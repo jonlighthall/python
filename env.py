05bfcdcc47310b3b2919239994e2ea35219a6a08
Jon Lighthall <jon.lighthall@gmail.com>
2021-04-16 12:32:16 -0500

    add print env var from Jun 20 2020

---
+++ env.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os
+ 
+# printing environment variables
+print(os.environ)
+print(os.environ['HOME'])
+if 'THISVAR' in os.environ:
+    print("this var found")
+    print(os.environ['THISVAR'])
+else:
+    print("this var not found")
