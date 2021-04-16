6897643e0e268ef493ceb35226760aa863f0bf1f
Jon Lighthall <jon.lighthall@gmail.com>
2021-04-16 12:37:24 -0500

    add prints

--- arb.py
+++ arb.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python
+print("demonstrantion of arbitray integer precision")
+print("this may take a while...")
 y = str( 5**4**3**2 )
 print ("5**4**3**2 = %s...%s and has %i digits" % (y[:20], y[-20:],len(y)))
